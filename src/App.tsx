30c450d5910be25f852a0ef2d0388a5756525abb
zeskord <zeskord@gmail.com>
2022-11-26 13:53:35 +0300

    Телеграм тест

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
+
 function App() {
 
   const style = {
     minWidth: '100vw',
     minHeight: '100vh'
   }
+  const webApp = Telegram.WebApp
+  const initDataUnsafe = webApp.initDataUnsafe
 
   return (
     <div className="bg-light" style={style}>
       <div className="container-sm">
         <p>Тест</p>
+        <p>{initDataUnsafe.toString()}</p>
       </div>
     </div>
 
