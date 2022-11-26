c7dc708ded1d554c5dda934e5594114d40581768
zeskord <zeskord@gmail.com>
2022-11-26 21:13:43 +0300

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ function App() {
   }
   const webApp = Telegram.WebApp
   const initDataUnsafe = webApp.initDataUnsafe
+  const webAppUser = initDataUnsafe.user
 
   return (
     <div className="bg-light" style={style}>
       <div className="container-sm">
         <p>Тест</p>
-        <p>{JSON.stringify(initDataUnsafe)}</p>
+        <p>{JSON.stringify(initDataUnsafe, undefined, "    ")}</p>
+        <p>{webAppUser?.photo_url}</p>
       </div>
     </div>
 
